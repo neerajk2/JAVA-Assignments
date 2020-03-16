de14f5cdb731ea358add843bd6acdf056e4fe6d2
neerajk2 <60866470+neerajk2@users.noreply.github.com>
2020-03-16 10:09:20 +0530

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,47 @@
+/**
+ * Create a java program to search through the home directory and
+ * look for files that match a regular expression.
+ * The program should be able to take inputs repeatedly and
+ * should print out the full absolute path of the matching files found.
+ */
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Assignment1 {
+    /**
+     * finds the file with a given regular expression, and path directory
+     * by default it will take path as home directory
+     * @param regex
+     * @param path
+     */
+    public void findFile (String regex, String path) {
+        File f = new File(path);
+        if (f == null) {
+            System.out.println("No files here");
+        } else {
+            File[] files = f.listFiles();
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    findFile(regex, file.getAbsolutePath());
+                }
+                if (file.getName().matches(regex)) {
+                    System.out.println("Files with regex " + regex + " has absolute path " + file.getAbsolutePath());
+                }
+            }
+        }
+    }
+
+    public static void main (String[] args) {
+        Assignment1 a1 = new Assignment1();
+        String home = System.getProperty("user.home");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a regex: ");
+        String regex = "";
+        while (!(regex = input.next()).equals("-1")){
+            a1.findFile(regex, home);
+            System.out.println("Enter a regex: ");
+        }
+        System.out.println("Program terminated");
+    }
+}
